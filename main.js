922d0e247ac4e039d7853f3690911a06a691864e
Deven Gurung <deven9229@gmail.com>
2022-04-05 15:29:32 +0600

    finished with the game

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Scores 
+let computerScore = 0;
+let playerScore = 0;
+
 // Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay() {
   const computerOptions = ['rock', 'paper', 'scissors'];
@@ -13,34 +17,50 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === 'rock') {
     if (computerSelection === 'scissors') {
+      playerScore++;
       return 'You win! Rock beats scissors';
     }else {
+      computerScore++;
       return 'You lose! Paper beats rock';
     }
   }
 
   if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
+      playerScore++;
       return 'You win! Paper beats rock';
     }else{
+      computerScore++;
       return 'You lose! Scissors beats paper';
     }
   }
 
    if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
+      computerScore++;
       return 'You lose! Rock beats scissors';
     }else{
+      playerScore++;
       return 'You win! Scissors beats paper';
     }
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
+function finalScore() {
+  if (playerScore >= 3) {
+    console.log("Player wins!");
+  }else{
+    console.log("Computer wins!");
   }
 }
 
-const playerSelection = "paper";
-const computerSelection = computerPlay();
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt('What do you choose?', ''.toLowerCase());
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  finalScore();
+}
+
+game();
